7e2fae66f69a83e6bb179ba5c00f1bb18cca3095
mkgupta01 <mk4693480@gmail.com>
2023-07-20 01:04:20 +0530

    Color coding pdf added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    colors:{
+      'dark-purple':'30011E',
+      'tea-green':'D7FCD4',
+      'celadon':'B6CCA1',
+      'dark-goldenred':'B68F40',
+      'davy-gray':'545454'
+    },
     extend: {
       fontFamily: {
         base: ['"Tektur, cursive"']
